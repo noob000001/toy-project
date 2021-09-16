18f8baabb4e24eb0a3cef8c0b48a53c6ed1cb2f3
noob000001 <kcdpads3@naver.com>
2021-09-16 23:32:55 +0900

    개별과제

--- src/com/kh/toy/common/exception/DataAccessException.java
+++ src/com/kh/toy/common/exception/DataAccessException.java
@@ -5,7 +5,7 @@ import com.kh.toy.common.code.ErrorCode;
 //예외처리가 강제되지 않는 UnCheckedException
 public class DataAccessException extends HandlableException{
 
-	private static final long serialVersionUID = -6445202136333072827L;
+	private static final long serialVersionUID = 1L;
 
 	public DataAccessException(Exception e) {
 		super(ErrorCode.DATABASE_ACCESS_ERROR,e);
